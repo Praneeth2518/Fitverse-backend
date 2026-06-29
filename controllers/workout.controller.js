7c40aceaf7ff8e189c39bf9c05ed91bbc27d317f
Praneeth <praneethgh16@gmail.com>
2026-06-29 21:45:31 +0530

    fix : workout controller

--- controllers/workout.controller.js
+++ controllers/workout.controller.js
@@ -1,9 +1,25 @@
+import ApiError from "../classes/ApiError.class.js";
+import Workout from "../models/workout.model.js";
+
 export async function getAllWorkouts(req, res, next) {
 
 }
 
 export async function createWorkout(req, res, next) {
+    try {
+        req.body.ownerId = req.user._id;
+        req.body.owner = req.user.displayName;
+        console.log(req.body);
+        const newWorkout = await Workout.create(req.body);
 
+        res.status(201).send({
+            success: true,
+            message: "Workout created successfully",
+            data: newWorkout
+        });
+    } catch (e) {
+        next(e);
+    }
 }
 
 export async function getWorkout(req, res, next) {
